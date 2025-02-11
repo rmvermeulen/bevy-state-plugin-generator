a56624a7ddef1fee4d4dee6c6b9e7fc8e60a1ece
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2025-02-11 21:33:18 +0100

    remove self-dep

--- build.rs
+++ build.rs
@@ -1,11 +1,3 @@
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(coverage_nightly)");
-
-    #[cfg(feature = "dogfood")]
-    bevy_state_plugin_generator::on_build_generate_plugin(
-        "examples/states.txt",
-        "examples/generated_states.rs",
-        Default::default(),
-    )
-    .expect("Failed to generate plugin");
 }
