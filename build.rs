3cde4df42776fce3f549e6078e6e3025b0a065e9
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2025-02-13 20:35:03 +0100

    add pre-{commit,push} hooks, configure in build.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,18 @@
+use std::process::Command;
+
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(coverage_nightly)");
+    let current_hooks = Command::new("git")
+        .args(["config", "--local", "core.hookPath"])
+        .output()
+        .unwrap();
+    let current_hooks = String::from_utf8(current_hooks.stdout).unwrap();
+    if current_hooks.trim() != "hooks" {
+        let hooks_ok = Command::new("git")
+            .args(["config", "--local", "core.hookPath", "hooks"])
+            .status()
+            .unwrap()
+            .success();
+        assert!(hooks_ok);
+    }
 }
