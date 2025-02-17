d91867686be301b83e16bb937614c906fd49095c
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2025-02-17 11:42:30 +0100

    fix typo build.rs

--- build.rs
+++ build.rs
@@ -3,13 +3,13 @@ use std::process::Command;
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(coverage_nightly)");
     let current_hooks = Command::new("git")
-        .args(["config", "--local", "core.hookPath"])
+        .args(["config", "--local", "core.hooksPath"])
         .output()
         .unwrap();
     let current_hooks = String::from_utf8(current_hooks.stdout).unwrap();
     if current_hooks.trim() != "hooks" {
         let hooks_ok = Command::new("git")
-            .args(["config", "--local", "core.hookPath", "hooks"])
+            .args(["config", "--local", "core.hooksPath", "hooks"])
             .status()
             .unwrap()
             .success();
