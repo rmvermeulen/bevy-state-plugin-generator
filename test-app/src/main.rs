1a27db42a1e1fb3e9aca98f9bbf999109e4ebe8f
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2025-02-21 19:33:22 +0100

    test-app: use generated_states

--- test-app/src/main.rs
+++ test-app/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod generated_states;
+
+use bevy_app::{App, AppExit};
+use generated_states::GeneratedStatesPlugin;
+
+fn main() -> AppExit {
+    let mut app = App::new();
+
+    app.add_plugins(GeneratedStatesPlugin);
+
+    app.run()
 }
