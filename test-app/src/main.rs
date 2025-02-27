98cbf54717e05b9a07252e5cc42176bc55d6bc7d
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2025-02-27 12:45:50 +0100

    test-app: fix bevy dep

--- test-app/src/main.rs
+++ test-app/src/main.rs
@@ -1,12 +1,15 @@
 mod generated_states;
 
-use bevy_app::{App, AppExit};
+use bevy::{
+    prelude::{App, AppExit},
+    state::app::StatesPlugin,
+};
 use generated_states::GeneratedStatesPlugin;
 
 fn main() -> AppExit {
     let mut app = App::new();
 
-    app.add_plugins(GeneratedStatesPlugin);
+    app.add_plugins((StatesPlugin, GeneratedStatesPlugin));
 
     app.run()
 }
