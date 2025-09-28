850f0e4af4c1833a7fe3dd41a91ea6bc344eb5b3
Rasmus <rasmus_vermeulen@tutanota.com>
2025-09-28 13:32:48 +0200

    test-app: use features to enable generated modules

--- test-app/src/main.rs
+++ test-app/src/main.rs
@@ -1,5 +1,9 @@
+#[cfg(feature = "inline")]
+mod auto_generated_states;
+#[cfg(feature = "full")]
 mod generated_states_full;
 mod generated_states_none;
+#[cfg(feature = "short")]
 mod generated_states_short;
 
 use bevy::prelude::{App, AppExit};
