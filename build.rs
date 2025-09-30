b7641bcfe898e594dd214e39f3b9ae01edf29ece
Rasmus <rasmus_vermeulen@tutanota.com>
2025-09-30 16:56:04 +0200

    build.rs: setup hooks path

--- build.rs
+++ build.rs
@@ -1,3 +1,17 @@
+use std::io;
+use std::process::Command;
+
+fn configure_hooks_path(path: &str) -> io::Result<bool> {
+    Command::new("git")
+        .args(["config", "--local", "core.hooksPath", path])
+        .status()
+        .map(|code| code.success())
+}
+
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(coverage_nightly)");
+    assert!(
+        configure_hooks_path("hooks").unwrap(),
+        "configure_hooks_path: unexpected exit code"
+    );
 }
