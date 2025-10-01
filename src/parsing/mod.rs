7b9b45d06d8cecd75a641a2357923557ff5ef73e
Rasmus <rasmus_vermeulen@tutanota.com>
2025-10-01 15:09:07 +0200

    parsing: add todo about tree-less parsing

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -8,3 +8,5 @@ mod tokens;
 pub use parsers::comment;
 pub use parsers::{config_is_valid, parse_node, parse_states_text};
 pub(crate) use tokens::*;
+
+// TODO: investigate "tree-less parsing", see https://www.youtube.com/watch?v=NxiKlnUtyio
