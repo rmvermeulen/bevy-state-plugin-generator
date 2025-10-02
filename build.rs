30c4cd8bc252502c6162c4b2575b4a1b2152ac78
Rasmus <rasmus_vermeulen@tutanota.com>
2025-10-02 20:07:23 +0200

    build.rs: check cfg(feature = dev)

--- build.rs
+++ build.rs
@@ -1,18 +1,15 @@
-use std::io;
-use std::process::Command;
-
-fn configure_hooks_path(path: &str) -> io::Result<bool> {
-    Command::new("git")
-        .args(["config", "--local", "core.hooksPath", path])
-        .status()
-        .map(|code| code.success())
-}
-
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(coverage_nightly)");
-    #[cfg(debug_assertions)]
-    assert!(
-        configure_hooks_path("hooks").unwrap(),
-        "configure_hooks_path: unexpected exit code"
-    );
+    #[cfg(feature = "dev")]
+    {
+        use std::process::Command;
+        assert!(
+            Command::new("git")
+                .args(["config", "--local", "core.hooksPath", "hooks"])
+                .status()
+                .map(|code| code.success())
+                .unwrap(),
+            "configure_hooks_path: unexpected exit code"
+        );
+    }
 }
