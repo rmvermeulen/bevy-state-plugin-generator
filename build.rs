f9d68c5dd93bc22ff2f056229245d0d95f66c3a8
Rasmus <rasmus_vermeulen@tutanota.com>
2025-10-02 17:45:41 +0200

    build.rs: check cfg(debug_assertions)

--- build.rs
+++ build.rs
@@ -10,6 +10,7 @@ fn configure_hooks_path(path: &str) -> io::Result<bool> {
 
 fn main() {
     println!("cargo::rustc-check-cfg=cfg(coverage_nightly)");
+    #[cfg(debug_assertions)]
     assert!(
         configure_hooks_path("hooks").unwrap(),
         "configure_hooks_path: unexpected exit code"
