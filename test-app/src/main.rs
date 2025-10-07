3ffdb188a1e07641266ef548a81e42807c8a0570
Rasmus <rasmus_vermeulen@tutanota.com>
2025-10-07 11:57:06 +0200

    test-app: add #[allow(dead_code)] to generated modules

--- test-app/src/main.rs
+++ test-app/src/main.rs
@@ -1,14 +1,18 @@
 cfg_if::cfg_if! {
     if #[cfg(feature = "inline")] {
+        #[allow(dead_code)]
         mod auto_generated_states;
         use auto_generated_states as generated_states;
     } else if #[cfg(feature = "full")] {
+        #[allow(dead_code)]
         mod generated_states_full;
         use generated_states_full as generated_states;
     } else if #[cfg(feature = "short")] {
+        #[allow(dead_code)]
         mod generated_states_short;
         use generated_states_short as generated_states;
     } else {
+        #[allow(dead_code)]
         mod generated_states_none;
         use generated_states_none as generated_states;
     }
